ac368f40049e9f276a1d2d78f37e1fd2f4c1956a
Marc Martinez <Marc Martinez>
2016-05-05 17:29:30 +0200

    ewsfew

--- TTP/routes/index.js
+++ TTP/routes/index.js
@@ -3,29 +3,32 @@ var bignum = require('bignum');
 var rsa = require('../rsa/rsa-bignum');
 var path = require('path');
 var router = express.Router();
-var keys = rsa.generateKeys(1024);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', 'ttp.html'));
+var keys = rsa.generateKeys(1024, function (keys) {
+    console.log("Keys are ready!");
 });
 
-router.post('/', function(req, res, next) {
- console.log(req.body);
- var blind = bignum(req.body.blind);
- var d = keys.privateKey.d.toString();
- var n = keys.publicKey.n.toString();
- console.log("LA clave privada del servidor d: ", d);
- console.log("LA n: ", n);
- console.log(blind);
- //AQUI NO ME VA me salta que la bc.powm no es una funcion --que raro-- en pallier me pasa lo mismo//
- var teta = keys.privateKey.encrypt(blind);
- console.log(teta);
- //SI FUNCIONA LO ANTERIOR EL SERVIDOR TE DA UNA FIRMA VALIDA"
- var sign = {
-     teta: teta.toString()
- };
- res.send(JSON.stringify(sign));
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../public', 'ttp.html'));
+});
+
+router.post('/', function (req, res, next) {
+    console.log(req.body);
+    var blind = bignum(req.body.blind);
+    var d = keys.privateKey.d.toString();
+    var n = keys.publicKey.n.toString();
+    console.log("LA clave privada del servidor d: ", d);
+    console.log("LA n: ", n);
+    console.log(blind);
+    //AQUI NO ME VA me salta que la bc.powm no es una funcion --que raro-- en pallier me pasa lo mismo//
+    var teta = keys.privateKey.encrypt(blind);
+    console.log(teta);
+    //SI FUNCIONA LO ANTERIOR EL SERVIDOR TE DA UNA FIRMA VALIDA"
+    var sign = {
+        teta: teta.toString()
+    };
+    res.send(JSON.stringify(sign));
 });
 
 router.get('/publicKey', function (req, res, next) {
